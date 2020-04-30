f4017f682f13d792f935028f247fae4c6679cc9e
Vladiyss <41474474+Vladiyss@users.noreply.github.com>
2020-04-30 13:57:46 +0300

    Deleted Nav document

--- Nav.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="./new1css.css" type="text/css" />
-<nav class="nav">
-			<a class="nav__link" href="new1.html"><p class=<?php echo $_SERVER['SCRIPT_NAME'] === 'new1.php' ? 'active' : '';?>>Главная</p></a>
-			<a class="nav__link" href="Records.html">Рекорды</a>
-			<a class="nav__link" href="Antirecords.html">Антирекорды<sup class="sup">new</sup></a>
-			<a class="nav__link" href="SiteDescription.html">Описание сайта</a>
-		</nav>
